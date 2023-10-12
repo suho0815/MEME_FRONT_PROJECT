a82f1f0251f025f2bb6519dc2f788de0d0cf391c
suho <suho0815@naver.com>
2023-10-12 15:56:51 +0900

    test: Modal 테스트

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,7 +1,11 @@
 /** 메인 페이지 */
 import {StyledDiv, StyledButton} from '../../styled'
+import {Modal} from '../../components'
+import {useState} from 'react'
 
 export const MainPage = () => {
+  const [open, setOpen] = useState(true)
+
   return (
     <main>
       <StyledDiv font="RedRose" bold="300">
@@ -10,6 +14,9 @@ export const MainPage = () => {
       <StyledButton color="white" background="#7929FF">
         hihi
       </StyledButton>
+      <Modal open={open} closeModal={setOpen}>
+        <div>모달 테스트입니다.</div>
+      </Modal>
     </main>
   )
 }
