685344b27aa2464c795565f0cd33e26e8b16becd
suho <suho0815@naver.com>
2023-10-12 13:30:49 +0900

    test: 라우팅 테스트

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,4 +1,4 @@
 /** 로그인 페이지 */
 export const Login = () => {
-  return <div></div>
+  return <div>로그인</div>
 }
--- src/pages/Auth/SignUp.js
+++ src/pages/Auth/SignUp.js
@@ -1,4 +1,4 @@
 /** 회원가입 페이지 */
 export const SignUp = () => {
-  return <div></div>
+  return <div>테스트 입니다.</div>
 }
