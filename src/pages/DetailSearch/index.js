8fa427ada2530c2690e11fc8e949ee56d19730f4
hong-sehyun <sese0733@naver.com>
2023-10-13 13:11:25 +0900

    FEAT: 상세 검색 페이지 추가

--- src/pages/DetailSearch/index.js
+++ src/pages/DetailSearch/index.js
@@ -1,6 +1,18 @@
 /** 상세검색 페이지 */
 export const DetailSearch = () => {
   return (
-    <h1>search</h1>
+    <section>
+      <div>
+        <h1>Discover the Popular Memes from a Specific Year!</h1>
+        <select>
+          <option>2023</option>
+          <option>2022</option>
+          <option>2021</option>
+        </select>
+        <input type="text"></input>
+        <button type="submit">검색</button>
+      </div>
+
+    </section>
   )
 }
