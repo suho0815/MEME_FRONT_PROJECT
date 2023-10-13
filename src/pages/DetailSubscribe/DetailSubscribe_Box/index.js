3b6d9c8309c7b354c68907ccf829ced35fbf7164
hong-sehyun <sese0733@naver.com>
2023-10-13 13:28:15 +0900

    Design: 구독 페이지 디자인 수정

--- src/pages/DetailSubscribe/DetailSubscribe_Box/index.js
+++ src/pages/DetailSubscribe/DetailSubscribe_Box/index.js
@@ -1,20 +1,31 @@
 import React from 'react'
-import { DetailSubscribe_BT as BT } from './DetailSubscribe_BT'
+// import { DetailSubscribe_BT as BT } from './DetailSubscribe_BT'
 import { DetailSubscribe_version as Version } from './DetailSubscribe_version'
-import { PriceDiv, VersionDiv } from '../../../styled/page'
+import { SubscribeDiv, VersionDiv, SubscribeBT, PriceDiv, Price } from '../../../styled/page'
 
 export const DetailSubscribe_Box = ({ version, price, month, discount, list }) => {
 
     return (
-        <PriceDiv>
-            <VersionDiv>
-                <Version version={version} />
-            </VersionDiv>
-            {price}
-            {month}
-            {discount}
+        <SubscribeDiv>
+            <div>
+                <VersionDiv>
+                    <Version version={version} />
+                </VersionDiv>
+            </div>
+            <PriceDiv>
+                <Price>
+                    {price}
+                </Price>
+                {month}
+                {discount}
+
+            </PriceDiv>
             {list}
-            <BT />
-        </PriceDiv>
+            <div>
+                <SubscribeBT>
+                    구독하기
+                </SubscribeBT>
+            </div>
+        </SubscribeDiv>
     )
 }
--- src/styled/page/DetailSubscribe.js
+++ src/styled/page/DetailSubscribe.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import theme from '../theme'
 
-export const PriceDiv = styled.div`
+export const SubscribeDiv = styled.div`
 border-radius: 10px;
 border: 1px solid #643579;
 `
@@ -12,5 +12,21 @@ border-radius: 5px;
 border: 1px solid #3D1860;
 margin: 20px;
 padding: 5px
+`
+export const SubscribeBT = styled.div`
+border-radius: 5px;
+display: inline-block; 
+background-color: #7929FF;
+padding:5px;
+color: white;
+margin: 20px;
+`
 
+export const PriceDiv = styled.div`
+font-family: 'Red Rose', serif;
+`
+
+export const Price = styled.div`
+font-size: x-large;
+font-weight: 700;
 `
