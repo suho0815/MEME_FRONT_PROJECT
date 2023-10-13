2e82b5d801e7774547fd466bb45991474d6ac4d6
hong-sehyun <sese0733@naver.com>
2023-10-13 13:10:33 +0900

    Design: 구독 페이지 디자인

---
+++ src/styled/page/DetailSubscribe.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import theme from '../theme'
+
+export const PriceDiv = styled.div`
+border-radius: 10px;
+border: 1px solid #643579;
+`
+
+export const VersionDiv = styled.div`
+display: inline-block; 
+border-radius: 5px;
+border: 1px solid #3D1860;
+margin: 20px;
+padding: 5px
+
+`
---
+++ src/styled/page/index.js
@@ -0,0 +1,3 @@
+// 페이지별 스타일 컴포넌트
+// export * from './Community'
+export * from './DetailSubscribe'
