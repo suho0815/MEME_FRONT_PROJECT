2c2b6987a4f7b1c262130db6f47855bf2c04ce56
suho0815 <suho0815@naver.com>
2023-10-16 14:44:54 +0900

    feat: 신고페이지 추가

--- src/pages/DetailCommunity/Community_Declaration/index.js
+++ src/pages/DetailCommunity/Community_Declaration/index.js
@@ -1,11 +1,41 @@
 /** 신고 페이지 */
-import {StyledTitle, StyledSection, StyledButton, NavBoxTest} from '../../../styled'
+import {BoardbtnDiv, StyledButton, StyledDiv, Title, NavBoxTest, CommentInput, BoardContentWrite} from '../../../styled'
+import {useNavigate} from 'react-router-dom'
+import {useRef} from 'react'
 
 export const Community_Declaration = () => {
+  const Navigate = useNavigate()
+  const titleRef = useRef(null)
+  const contentRef = useRef(null)
+
+  const SubmitBtnClicked = () => {
+    console.log(titleRef.current.value)
+    console.log(contentRef.current.value)
+  }
+  const CancleBtnClicked = () => {
+    Navigate(-1)
+  }
   return (
-    <StyledSection>
+    <section>
       <NavBoxTest></NavBoxTest>
-      <StyledTitle font="RedRose">신고</StyledTitle>
-    </StyledSection>
+      <div style={{display: 'flex', width: '50%', height: '150px', alignItems: 'center', justifyContent: 'center'}}>
+        <Title font="RedRose" margin="" padding="6px 12px">
+          신고하기
+        </Title>
+      </div>
+      <StyledDiv width="70%" style={{flexDirection: 'column'}}>
+        <CommentInput placeholder="신고 제목 입력하셈!" />
+        <select style={{marginTop: '25px'}}>
+          <option>부적절한 단어 사용</option>
+        </select>
+        <BoardContentWrite ref={contentRef} placeholder="신고 내용 입력하셈!" style={{marginTop: '25px'}} />
+      </StyledDiv>
+      <BoardbtnDiv justifycontents="center">
+        <StyledButton color="white" background="#7929FF" style={{marginRight: '30px'}} onClick={SubmitBtnClicked}>
+          신고하기
+        </StyledButton>
+        <StyledButton onClick={CancleBtnClicked}>취소하기</StyledButton>
+      </BoardbtnDiv>
+    </section>
   )
 }
