66e070f016ba4af32ba1787d1f6e1231a7143266
suho0815 <suho0815@naver.com>
2023-10-16 14:43:21 +0900

    fix: 경고 수정

--- src/pages/DetailCommunity/DetailCommunity_write/index.js
+++ src/pages/DetailCommunity/DetailCommunity_write/index.js
@@ -7,16 +7,41 @@ import {
   StyledDiv,
   CommentInput,
   BoardContentWrite,
-  BoardbtnDiv
+  BoardbtnDiv,
+  ImgInputlabel
 } from '../../../styled'
+import {Icon} from '../../../components'
 import {useNavigate} from 'react-router-dom'
+import {useState, useRef} from 'react'
 
 export const DetailCommunity_write = () => {
   const Navigate = useNavigate()
+  const [imgFile, setImgFile] = useState('')
+  const imgRef = useRef()
+  const titleRef = useRef(null)
+  const contentRef = useRef(null)
 
-  const SubmitBtnClicked = () => {}
+  const SubmitBtnClicked = () => {
+    console.log(titleRef.current.value)
+    console.log(contentRef.current.value)
+  }
   const CancleBtnClicked = () => {
     Navigate(-1)
+  }
+
+  // 이미지 업로드 input의 onChange
+  const ImgFileOnChange = () => {
+    const file = imgRef.current?.files[0]
+
+    if (file) {
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.onloadend = () => {
+        setImgFile(reader.result)
+      }
+    } else {
+      console.error('이미지가 선택되지 않았습니다.')
+    }
   }
 
   return (
@@ -24,22 +49,37 @@ export const DetailCommunity_write = () => {
       <NavBoxTest></NavBoxTest>
       <StyledTitle font="RedRose">Community</StyledTitle>
 
-      <StyledDiv width="70%" alignItems="left" style={{flexDirection: 'column'}}>
-        <CommentInput
-          width="40%"
-          placeholder="게시글 제목 입력하셈!"
-          style={{marginTop: '25px', marginBottom: '25px'}}
+      <StyledDiv width="70%" alignitems="left" style={{flexDirection: 'column'}}>
+        <StyledDiv width="100%" justifycontents="left">
+          <select style={{marginRight: '15px'}}>
+            <Icon name="person" />
+            <option>notice</option>
+          </select>
+          <CommentInput
+            ref={titleRef}
+            width="60%"
+            placeholder="게시글 제목 입력하셈!"
+            style={{marginTop: '25px', marginBottom: '25px'}}
+          />
+        </StyledDiv>
+
+        <ImgInputlabel htmlFor="imgupload">이미지 첨부</ImgInputlabel>
+        <input
+          type="file"
+          accept="image/*"
+          id="imgupload"
+          onChange={ImgFileOnChange}
+          ref={imgRef}
+          style={{display: 'none'}}
         />
-        <StyledButton color="white" font="poppins" background="#7929FF">
-          이미지 첨부
-        </StyledButton>
-        <BoardContentWrite placeholder="게시글 내용 입력하셈!" style={{marginTop: '25px'}} />
+        <img src={imgFile ? imgFile : ``} alt="이미지 미리보기" />
+        <BoardContentWrite ref={contentRef} placeholder="게시글 내용 입력하셈!" style={{marginTop: '25px'}} />
       </StyledDiv>
-      <BoardbtnDiv justifyContent="center">
-        <StyledButton color="" background="" style={{marginRight: '30px'}}>
+      <BoardbtnDiv justifycontents="center">
+        <StyledButton color="" background="" style={{marginRight: '30px'}} onClick={SubmitBtnClicked}>
           등록하기
         </StyledButton>
-        <StyledButton>취소하기</StyledButton>
+        <StyledButton onClick={CancleBtnClicked}>취소하기</StyledButton>
       </BoardbtnDiv>
     </StyledSection>
   )
